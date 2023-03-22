c2456c5b1df107dd20a856ae68280858e9ba1936
Egle Sabaliauskaite <59394960+esabaliauskaite@users.noreply.github.com>
2023-03-22 22:07:48 +0100

    project init commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Card from './Card';
+import reportWebVitals from './reportWebVitals';
+import 'tachyons';
+import { robots } from './Robots';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
