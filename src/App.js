4df6f35a6c9d787abc3d916e9e07ded7d46a30fb
Egle Sabaliauskaite <59394960+esabaliauskaite@users.noreply.github.com>
2023-03-24 21:18:44 +0100

    moving from local data to placeholder

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import CardList from "./CardList";
-import { robots } from './Robots';
 import SearchBox from './SearchBox';
 import './App.css';
 
@@ -8,9 +7,15 @@ class App extends Component {
     constructor () {
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: '',
         }
+    }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response =>  response.json())
+        .then(users => this.setState({ robots: users}));
     }
 
     onSearchChange = (event) => {
