9b252eff04b70ddc7e803fd6b868710a688dce1c
Egle Sabaliauskaite <59394960+esabaliauskaite@users.noreply.github.com>
2023-03-24 21:29:09 +0100

    add scroll functionality

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import CardList from "./CardList";
 import SearchBox from './SearchBox';
 import './App.css';
+import Scroll from './Scroll';
 
 class App extends Component {
     constructor () {
@@ -26,14 +27,19 @@ class App extends Component {
         const filteredRobots = this.state.robots.filter(robot => {
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
         });
-        console.log(filteredRobots);
-        return (
-            <div className="tc">
-                <h1 className="f1">RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange} />
-                <CardList robots={filteredRobots} />
-            </div>
-        );
+        if (this.state.robots.length === 0){
+            return <h1>Loading</h1>
+        } else {
+            return (
+                <div className="tc">
+                    <h1 className="f1">RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <Scroll>
+                        <CardList robots={filteredRobots} />
+                    </Scroll>
+                </div>
+            );
+        }
     }
 }
 
